fcc955a2449b8ce1fb8d4a0fcdb4f833dc0b0e50
Ben Zhou <zhougy0717@gmail.com>
2023-09-25 20:14:45 +0800

    Feature: Add Obsidian and VSCOde for Windows

---
+++ json/obsidian_windows.js
@@ -0,0 +1,130 @@
+let shortcuts = [
+    {
+        title: '当前行变为checklist ctrl + l',
+        description: '注意是字母l。在行首添加 - [ ]，如果已经是checklist，则会执行toggle动作。',
+        keyword: 'windows obsidian checklist todo',
+        keys: ['l', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '添加注释 ctrl + /',
+        description: '添加注释, 即%% %%',
+        keyword: 'windows obsidian comment 注释',
+        keys: ['/', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '插入markdown链接 ctrl + k',
+        description: '插入[](), 非[[]]',
+        keyword: 'windows obsidian markdown link 链接',
+        keys: ['k', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '切换到第一个tab ctrl + 1',
+        description: '切换到第一个tab',
+        keyword: 'windows obsidian 切换 tab 111',
+        keys: ['1', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '切换到第一个tab ctrl + 2',
+        description: '切换到第二个tab',
+        keyword: 'windows obsidian 切换 tab 222',
+        keys: ['2', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '切换到第一个tab ctrl + 3',
+        description: '切换到第三个tab',
+        keyword: 'windows obsidian 切换 tab 333',
+        keys: ['3', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '切换到第一个tab ctrl + 4',
+        description: '切换到第四个tab',
+        keyword: 'windows obsidian 切换 tab 444',
+        keys: ['4', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '切换到第一个tab ctrl + 5',
+        description: '切换到第五个tab',
+        keyword: 'windows obsidian 切换 tab 555',
+        keys: ['5', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '显示笔记列表 ctrl + o',
+        description: '显示笔记列表',
+        keyword: 'windows obsidian list 列表 open',
+        keys: ['o', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '后退 ctrl + alt + ⬅',
+        description: '后退',
+        keyword: 'windows obsidian 后退 back',
+        keys: ['left', 'ctrl', 'alt'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '前进 ctrl + alt + ⮕',
+        description: '前进',
+        keyword: 'windows obsidian 前进 forward',
+        keys: ['right', 'ctrl', 'alt'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '打开设置 ctrl + ,',
+        description: '打开设置',
+        keyword: 'windows obsidian 设置 setting config',
+        keys: [',', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '预览文件 ctrl',
+        description: '把光标放在文件管理器的某个文件上，弹出浮动窗口显示这个文件的内容。记住按住ctrl键',
+        keyword: 'windows obsidian preview 预览',
+        keys: ['ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '删除当前段落 ctrl + d',
+        description: '删除当前段落',
+        keyword: 'windows obsidian delete 删除段落',
+        keys: ['d', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '加粗 ctrl + b',
+        description: '设置粗体',
+        keyword: 'windows obsidian 加粗 粗体',
+        keys: ['b', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '斜体 ctrl + i',
+        description: '设置斜体',
+        keyword: 'windows obsidian 斜体',
+        keys: ['i', 'ctrl'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '当前文件查找并替换 ctrl + alt + f',
+        description: '当前文件查找并替换',
+        keyword: 'windows obsidian find replace 查找替换',
+        keys: ['f', 'ctrl', 'alt'],
+        icon: 'icons/obsidian.png'
+    },
+    {
+        title: '重新打开上一个被关闭的 tab ctrl + shift + t',
+        description: '重新打开上一个被关闭的 tab',
+        keyword: 'windows obsidian open closed tab last',
+        keys: ['t', 'ctrl', 'shift'],
+        icon: 'icons/obsidian.png'
+    }
+]
+
+module.exports = shortcuts
---
+++ json/vscode_windows.js
@@ -0,0 +1,109 @@
+let shortcuts = [
+    {
+        title: '代码格式化 alt + shift + f',
+        description: '格式化代码',
+        keyword: 'windows 格式化 format vscode visual studio',
+        keys: ['f', 'shift', 'alt'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '格式化光标所在行 ctrl + k ctrl + f',
+        description: '格式化光标所在行',
+        keyword: 'windows 格式化 format 行 capture vscode',
+        keys: [['k', 'ctrl'], ['f', 'ctrl']],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '缩进 ctrl + ]',
+        description: '缩进',
+        keyword: 'windows 格式化 缩进 indent vscode',
+        keys: [']', 'ctrl'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '减少缩进 ctrl + [',
+        description: '减少缩进',
+        keyword: 'windows 格式化 缩进 indent vscode',
+        keys: ['[', 'ctrl'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '注释代码块 alt + shift + a',
+        description: '注释代码块',
+        keyword: 'windows 格式化 注释 块 vscode',
+        keys: ['a', 'alt', 'shift'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '光标跳转到对应的括号 ctrl + shift + \\',
+        description: '光标跳转到对应的括号',
+        keyword: 'windows 光标 跳转 括号 vscode',
+        keys: ['\\', 'ctrl', 'shift'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '跳转到指定行 ctrl + g',
+        description: '跳转到指定行',
+        keyword: 'windows 光标 跳转 行 vscode',
+        keys: ['g', 'ctrl'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '显示当前文件函数列表 ctrl + shift + o',
+        description: '显示当前文件函数列表',
+        keyword: 'windows 光标 跳转 函数 vscode',
+        keys: ['o', 'ctrl', 'shift'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '显示打开文件列表 ctrl + shift + tab',
+        description: '显示打开文件列表',
+        keyword: 'windows 打开文件 跳转 vscode',
+        keys: ['tab', 'ctrl', 'shift'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '跳转到最近打开的文件 ctrl + -',
+        description: '跳转到最近打开的文件',
+        keyword: 'windows 光标 跳转 文件 上一个 最近 vscode',
+        keys: ['-', 'ctrl'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '在finder中显示当前文件 ctrl + k r',
+        description: '在finder中显示当前文件',
+        keyword: 'windows finder 文件 vscode',
+        keys: [['k', 'ctrl'], ['r']],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '显示控制台 ctrl + shift + p',
+        description: '显示控制台',
+        keyword: 'windows 控制台 console vscode',
+        keys: ['p', 'ctrl', 'shift'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '界面放大 ctrl + =',
+        description: '界面放大',
+        keyword: 'windows zoom in fangda vscode',
+        keys: ['=', 'ctrl'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '界面缩小 ctrl + -',
+        description: '界面缩小',
+        keyword: 'windows zoom out suoxiao vscode',
+        keys: ['-', 'ctrl'],
+        icon: 'icons/vscode.png'
+    },
+    {
+        title: '全部保存 ctrl + shift + s',
+        description: '全部保存',
+        keyword: 'windows quanbu baocun save all vscode',
+        keys: ['s', 'ctrl', 'alt'],
+        icon: 'icons/vscode.png'
+    }
+]
+
+module.exports = shortcuts

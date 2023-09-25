e87a70bea8a665715e771a1e88a7c3287f2b21d4
Ben Zhou <zhougy0717@gmail.com>
2023-09-26 06:43:08 +0800

    Feature: Add more keyword for system hotkeys and add some more useful hotkeys

--- json/macos.js
+++ json/macos.js
@@ -2,225 +2,239 @@ let shortcuts = [
     {
         title: '全屏截图 cmd + shift + 3',
         description: '直接截取整个屏幕，并保存成文件，默认存放在桌面',
-        keyword: 'macos 全屏截图 jietu capture',
+        keyword: 'macos system 全屏截图 jietu capture',
         keys: ['3', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '区域截图 cmd + shift + 4',
         description: '截取屏幕选取，并保存成文件，默认存放在桌面',
-        keyword: 'macos 区域截图 jietu capture',
+        keyword: 'macos system 区域截图 jietu capture',
         keys: ['4', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '屏幕捕获工具 cmd + shift + 5',
         description: '开启自带屏幕捕获工具，可以截取或录制屏幕',
-        keyword: 'macos 屏幕捕获工具 录制屏幕 jietu capture',
+        keyword: 'macos system 屏幕捕获工具 录制屏幕 jietu capture',
         keys: ['5', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '撤销 cmd + z',
         description: '撤销',
-        keyword: 'macos revert chexiao',
+        keyword: 'macos system revert chexiao',
         keys: ['z', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '重做 cmd + shift + z',
         description: '重做',
-        keyword: 'macos redo chongzuo revert',
+        keyword: 'macos system redo chongzuo revert',
         keys: ['z', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '强制退出App cmd + shift + esc',
         description: '强制退出App, 另一种方法是cmd + q',
-        keyword: 'macos force qiangzhi quit tuichu',
+        keyword: 'macos system force qiangzhi quit tuichu',
         keys: ['esc', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '显示字符检视器 cmd + ctrl + space',
         description: '显示字符检视器，你可以从中选取表情符号和其他符号',
-        keyword: 'macos emoji 特殊字符 teshu zifu',
+        keyword: 'macos system emoji 特殊字符 teshu zifu',
         keys: ['space', 'command', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '创建文件夹 cmd + shift + n',
         description: '在访达中创建一个新文件夹',
-        keyword: 'macos finder mulu 目录 wenjianjia create',
+        keyword: 'macos system finder mulu 目录 wenjianjia create',
         keys: ['n', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '打印 cmd + p',
         description: '打印',
-        keyword: 'macos print dayin',
+        keyword: 'macos system print dayin',
         keys: ['p', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '打开任务管理器 cmd + option + esc',
         description: '打开任务管理器，无法自动触发，需要手动按下对应的按键',
-        keyword: 'macos renwu guanli task manager',
+        keyword: 'macos system renwu guanli task manager',
         keys: ['esc', 'command', 'option'],
         icon: 'icons/macos.png'
     },
     {
         title: '强制退出App cmd + q',
         description: '强制退出App',
-        keyword: 'macos force qiangzhi quit tuichu',
+        keyword: 'macos system force qiangzhi quit tuichu',
         keys: ['q', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '锁定屏幕 cmd + ctrl + q',
         description: '锁定屏幕',
-        keyword: 'macos lock suoding pingmu',
+        keyword: 'macos system lock suoding pingmu',
         keys: ['q', 'command', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '显示隐藏文件 cmd + shift + .',
         description: '显示隐藏文件',
-        keyword: 'macos hide yincang wenjian',
+        keyword: 'macos system hide yincang wenjian',
         keys: ['.', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: 'App全屏 cmd + ctrl + f',
         description: '如果App支持，全屏显示该App',
-        keyword: 'macos quanping',
+        keyword: 'macos system quanping',
         keys: ['f', 'command', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '显示调度中心 ctrl + ⬆',
         description: '显示调度中心',
-        keyword: 'macos diaoduzhongxin schedule',
+        keyword: 'macos system diaoduzhongxin schedule',
         keys: ['up', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '显示当前应用窗口 ctrl + ⬇',
         description: '显示当前应用窗口',
-        keyword: 'macos dangqianchuangkou current focus window',
+        keyword: 'macos system dangqianchuangkou current focus window',
         keys: ['down', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '带样式拷贝 cmd + option + c',
         description: '带样式拷贝',
-        keyword: 'macos yangshi style copy kaobei',
+        keyword: 'macos system yangshi style copy kaobei',
         keys: ['c', 'command', 'option'],
         icon: 'icons/macos.png'
     },
     {
         title: '带样式粘贴 cmd + option + v',
         description: '带样式粘贴',
-        keyword: 'macos yangshi style paste zhantie niantie',
+        keyword: 'macos system yangshi style paste zhantie niantie',
         keys: ['v', 'command', 'option'],
         icon: 'icons/macos.png'
     },
     {
         title: '直接强制退出当前软件 cmd + option + shift + esc',
         description: '直接强制退出当前软件, 效果等同于cmd + q, 更野蛮',
-        keyword: 'macos force quit qiangzhi tuichu',
+        keyword: 'macos system force quit qiangzhi tuichu',
         keys: ['esc', 'command', 'option', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '隐藏当前软件 cmd + h',
         description: '隐藏当前软件',
-        keyword: 'macos yincang hide',
+        keyword: 'macos system yincang hide',
         keys: ['h', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到全部文本开头 cmd + ⬆',
         description: '光标移动到全部文本开头',
-        keyword: 'macos guangbiao cursor begin start',
+        keyword: 'macos system guangbiao cursor begin start',
         keys: ['up', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到全部文本结尾 cmd + ⬇',
         description: '光标移动到全部文本结尾',
-        keyword: 'macos guangbiao cursor end',
+        keyword: 'macos system guangbiao cursor end',
         keys: ['down', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到当前行结尾 cmd + ⮕',
         description: '光标移动到当前行结尾',
-        keyword: 'macos guangbiao cursor line end',
+        keyword: 'macos system guangbiao cursor line end',
         keys: ['right', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到当前行开头 cmd + ⬅',
         description: '光标移动到当前行开头',
-        keyword: 'macos guangbiao cursor line start',
+        keyword: 'macos system guangbiao cursor line start',
         keys: ['left', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到当前段落开头 ctrl + a',
         description: '光标移动到当前段落开头',
-        keyword: 'macos duanluo kaitou begin start block',
+        keyword: 'macos system duanluo kaitou begin start block',
         keys: ['a', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到当前段落结尾 ctrl + e',
         description: '光标移动到当前段落结尾',
-        keyword: 'macos duanluo jiewei end block',
+        keyword: 'macos system duanluo jiewei end block',
         keys: ['e', 'ctrl'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到当前单词的开头 option + ⬅',
         description: '光标移动到当前单词的开头',
-        keyword: 'macos danci kaitou begin start word',
+        keyword: 'macos system danci kaitou begin start word',
         keys: ['left', 'option'],
         icon: 'icons/macos.png'
     },
     {
         title: '光标移动到当前单词的结尾 option + ⮕',
         description: '光标移动到当前单词的结尾',
-        keyword: 'macos danci jiewei end word',
+        keyword: 'macos system danci jiewei end word',
         keys: ['right', 'option'],
         icon: 'icons/macos.png'
     },
     {
         title: 'Finder中复制文件夹路径 cmd + option + c',
         description: 'Finder中复制文件夹路径',
-        keyword: 'macos lujing path fuzhi copy',
+        keyword: 'macos system lujing path fuzhi copy',
         keys: ['c', 'command', 'option'],
         icon: 'icons/macos.png'
     },
     {
         title: '显示简介 cmd + i',
         description: '显示简介',
-        keyword: 'macos jianjie info finder',
+        keyword: 'macos system jianjie info finder',
         keys: ['i', 'command'],
         icon: 'icons/macos.png'
     },
     {
         title: '前往文件夹 cmd + shift + g',
         description: '前往文件夹',
-        keyword: 'macos qianwang wenjianjia folder cd directory finder',
+        keyword: 'macos system qianwang wenjianjia folder cd directory finder',
         keys: ['g', 'command', 'shift'],
         icon: 'icons/macos.png'
     },
     {
         title: '复制所选文件 cmd + d',
         description: '复制所选文件',
-        keyword: 'macos fuzhi copy selected finder',
+        keyword: 'macos system fuzhi copy selected finder',
         keys: ['d', 'command'],
+        icon: 'icons/macos.png'
+    },
+    {
+        title: '将所选项移入废纸篓 cmd + backspace',
+        description: '复制所选文件',
+        keyword: 'macos system feizhilou dustbin shanchu delete finder',
+        keys: ['backspace', 'command'],
+        icon: 'icons/macos.png'
+    },
+    {
+        title: '清倒废纸篓 cmd + shift + backspace',
+        description: '清倒废纸篓',
+        keyword: 'macos system feizhilou dustbin qingdao delete all finder',
+        keys: ['backspace', 'command', 'shift'],
         icon: 'icons/macos.png'
     }
 ]
--- json/windows.js
+++ json/windows.js
@@ -2,49 +2,49 @@ let shortcuts = [
     {
         title: '锁定电脑 win + l',
         description: '注意是字母l',
-        keyword: 'windows lock',
+        keyword: 'windows xitong  system lock',
         keys: ['l', 'windows'],
         icon: 'icons/windows.png'
     },
     {
         title: '复制文件夹路径 ctrl + shift + c',
         description: '复制文件夹路径',
-        keyword: 'windows copy folder directory fuzhi wenjianjia lujing',
+        keyword: 'windows xitong  system copy folder directory fuzhi wenjianjia lujing',
         keys: ['ctrl', 'shift', 'c'],
         icon: 'icons/windows.png'
     },
     {
         title: '查看文件或文件夹属性 alt + enter',
         description: '查看文件或文件夹属性',
-        keyword: 'windows shuxing property file folder wenjianjia',
+        keyword: 'windows xitong  system shuxing property file folder wenjianjia',
         keys: ['alt', 'enter'],
         icon: 'icons/windows.png'
     },
     {
         title: '前往目录 f4',
         description: '前往目录',
-        keyword: 'windows qianwang mulu cd directory shuru lujing path input',
+        keyword: 'windows xitong  system qianwang mulu cd directory shuru lujing path input',
         keys: ['f4'],
         icon: 'icons/windows.png'
     },
     {
         title: '打开文件浏览器 win + e',
         description: '打开文件浏览器',
-        keyword: 'windows file explorer wenjian liulanqi',
+        keyword: 'windows xitong  system file explorer wenjian liulanqi',
         keys: ['e', "windows"],
         icon: 'icons/windows.png'
     },
     {
         title: '收起所有窗口 win + m',
         description: '收起所有窗口',
-        keyword: 'windows shouqi',
+        keyword: 'windows xitong  system shouqi',
         keys: ['m', "windows"],
         icon: 'icons/windows.png'
     },
     {
         title: '放大镜 win + ++++',
         description: '放大镜, 猛戳加号',
-        keyword: 'windows fangdajing zoom',
+        keyword: 'windows xitong  system fangdajing zoom',
         keys: [['+', "windows"],['+', "windows"],['+', "windows"],['+', "windows"]],
         icon: 'icons/windows.png'
     }
